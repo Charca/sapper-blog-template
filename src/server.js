80c2110c1cd5e16b5ecf80ae66bdcc48ea2ac44b
Andre JS <andre.siedersleben@gmail.com>
2020-07-04 09:46:15 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,23 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
-import * as sapper from '@sapper/server';
+import sirv from 'sirv'
+import compression from 'compression'
+import * as sapper from '@sapper/server'
+import polka from 'polka'
+import helmet from 'helmet'
+import volleyball from 'volleyball'
 
-const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const { PORT, NODE_ENV } = process.env
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+const dev = NODE_ENV === 'development'
+
+polka()
+    .use(volleyball)
+    .use(helmet())
+    .use(
+	compression({ threshold: 0 }),
+	sirv('static', { dev }),
+	sapper.middleware()
+    )
+    .listen(PORT, (err) => {
+	if (err) { console.log('error', err); throw err }
+	/* else */ console.log(`🐱‍🏍 Server listening on port ${PORT}`)
+    })
