09d1afb8f489a38e0d8037aad5f7421bd631ed2b
Andre JS <andre.siedersleben@gmail.com>
2020-07-04 09:39:03 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,24 @@
-import resolve from 'rollup-plugin-node-resolve';
-import replace from 'rollup-plugin-replace';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
-import babel from 'rollup-plugin-babel';
+import sveltePreprocess from 'svelte-preprocess';
+import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
-import config from 'sapper/config/rollup.js';
+
 import marked from 'marked';
-import pkg from './package.json';
 
-const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
-const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+import config from 'sapper/config/rollup'
+import pkg from './package.json'
 
-const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && warning.message.includes('/@sapper/')) || onwarn(warning);
+const mode = process.env.NODE_ENV
+const dev = mode === 'development'
+const legacy = !!process.env.SAPPER_LEGACY_BUILD
+
+const onwarn = (warning, onwarn) => (
+	warning.code === 'CIRCULAR_DEPENDENCY'
+	&& /[/\\]@sapper[/\\]/.test(warning.message) // eslint-disable-line
+) || onwarn(warning)
 
 const markdown = () => ({
 	transform (md, id) {
@@ -29,43 +35,20 @@ export default {
 		input: config.client.input(),
 		output: config.client.output(),
 		plugins: [
-			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-
-			svelte({
-				dev,
-				hydratable: true,
-				emitCss: true
-			}),
-
-			resolve(),
-
+			replace({ 'process.browser': true, 'process.env.NODE_ENV': JSON.stringify(mode) }),
+			svelte({ preprocess: sveltePreprocess(), dev, hydratable: true, emitCss: true }),
+			resolve({ browser: true, dedupe: ["svelte"] }),
 			commonjs(),
-
 			legacy && babel({
-				extensions: ['.js', '.mjs', '.html', '.svelte'],
-				runtimeHelpers: true,
-				exclude: ['node_modules/@babel/**'],
-				presets: [
-					['@babel/preset-env', {
-						targets: '> 0.25%, not dead'
-					}]
-				],
-				plugins: [
-					'@babel/plugin-syntax-dynamic-import',
-					['@babel/plugin-transform-runtime', {
-						useESModules: true
-					}]
-				]
+				extensions: [".js", ".mjs", ".html", ".svelte"],
+				babelHelpers: "runtime",
+				exclude: ["node_modules/@babel/**"],
+				presets: [["@babel/preset-env", { targets: "> 0.2%, not dead, not ie <= 11, not op_mini all" }]],
+				plugins: ["@babel/plugin-syntax-dynamic-import", ["@babel/plugin-transform-runtime", { useESModules: true }]]
 			}),
-
-			!dev && terser({
-				module: true
-			})
+			!dev && terser({ module: true })
 		],
-
+		preserveEntrySignatures: false,
 		onwarn
 	},
 
@@ -73,26 +56,16 @@ export default {
 		input: config.server.input(),
 		output: config.server.output(),
 		plugins: [
-			replace({
-				'process.browser': false,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-
-			svelte({
-				generate: 'ssr',
-				dev
-			}),
-
-			resolve(),
-
+			replace({ "process.browser": false, "process.env.NODE_ENV": JSON.stringify(mode) }),
+			svelte({ generate: "ssr", dev }),
+			resolve({ dedupe: ["svelte"] }),
 			commonjs(),
-
 			markdown()
 		],
 		external: Object.keys(pkg.dependencies).concat(
 			require('module').builtinModules || Object.keys(process.binding('natives'))
 		),
-
+		preserveEntrySignatures: 'strict',
 		onwarn
 	},
 
@@ -101,16 +74,15 @@ export default {
 		output: config.serviceworker.output(),
 		plugins: [
 			resolve(),
-
 			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
+				"process.browser": true,
+				"process.env.NODE_ENV": JSON.stringify(mode)
 			}),
-
 			commonjs(),
 			!dev && terser()
 		],
 
+		preserveEntrySignatures: false,
 		onwarn
 	}
 };
