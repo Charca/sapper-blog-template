7b6fe2da9f92bd1bad3266a81c8bb139e0ce61cf
Andre JS <andre.siedersleben@gmail.com>
2020-07-07 13:32:58 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,7 +9,10 @@ const { PORT, NODE_ENV } = process.env || 5000
 
 const dev = NODE_ENV === 'development'
 
-polka()
+const app = polka()
+
+if (dev) {
+    app
 	.use(!dev ? volleyball.custom({ debug: true }) : volleyball)
 	.use(helmet())
 	.use(compression({ threshold: 0 }), sirv('static', { dev }), sapper.middleware())
@@ -21,3 +24,8 @@ polka()
 		// else
 		console.log(`🎈 Server listening on port ${PORT}`)
 	})
+} else {
+  app.use(sapper.middleware())
+}
+
+export default app
