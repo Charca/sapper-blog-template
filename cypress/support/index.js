26ab2a69453558d7fb402385e53af186b9b9cbdd
Andre Siedersleben <andre.siedersleben@meinestadt.de>
2020-07-10 02:44:27 +0200

    Add cypress code-coverage

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -13,5 +13,5 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-// Import commands.js using ES2015 syntax:
-import './commands'
+import './logger'
+import '@cypress/code-coverage/support'
---
+++ cypress/support/logger.js
@@ -0,0 +1,26 @@
+// ************************************************
+// Check docs:https://on.cypress.io/custom-commands
+// ************************************************
+
+/**
+ * Consistently logs the given string to the Command Log
+ * so the user knows the log message is coming from this plugin.
+ * @param {string} s Message to log.
+ */
+const logMessage = s => {
+  cy.log(`${s} \`[@cypress/code-coverage]\``)
+}
+
+// -- This is a parent command --
+Cypress.Commands.add("login", (email, password) =>
+	!email || !password ? logMessage(`User "${email}" had no password 😨`) : logMessage(`User "${email}" logged in 📣`)
+})
+
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+
+// -- This is will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
