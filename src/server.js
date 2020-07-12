5436f7fb039fdde6947d8722f5e40b2e4b3b16f0
jsbase <68040289+jsbase@users.noreply.github.com>
2020-07-12 05:03:30 +0200

    Fix cypress (#50)
    Co-authored-by: Andre Siedersleben <andre.siedersleben@meinestadt.de>

--- src/server.js
+++ src/server.js
@@ -1,27 +1,29 @@
-import sirv from "sirv";
-import compression from "compression";
+require("dotenv").config();
 import * as sapper from "@sapper/server";
-import polka from "polka";
+import compression from "compression";
 import helmet from "helmet";
+import polka from "polka";
+import sirv from "sirv";
 import volleyball from "volleyball";
 
-const { PORT, NODE_ENV } = process.env || 5000;
+const { PORT, NODE_ENV } = process.env;
 
 const dev = NODE_ENV === "development";
 
 polka()
-  .use(volleyball.custom({ debug: true }))
+  .use(volleyball.custom())
   .use(helmet())
   .use(
     compression({ threshold: 0 }),
     sirv("static", { dev }),
     sapper.middleware()
   )
-  .listen(PORT, (err) => {
+  .listen(PORT || 5000, (err) => {
     if (err) {
       console.log("error", err);
       throw err;
     }
 
+    console.log(JSON.stringify(process.env.CYPRESS_PROJECT_ID));
     console.log(`🎈 Server listening on port ${PORT}`);
   });
